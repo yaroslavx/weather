4fdc366cdfdbba6dd25edba88088268f7acef4e1
yaroslavx <yakupov.yaroslav@icloud.com>
2022-07-15 23:55:43 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
+import { CSSTransition } from "react-transition-group";
 
 
 export default function App() { 
@@ -10,40 +11,41 @@ export default function App() {
   
   const getData = (response) => {
     setData(response)
+    console.log(data)
   }
 
   return (
     <div className='app'>
       <Search getData={getData}/>
-      <div className="container">
-        <div className="top">
-          <div className="location">
-            {(data.name) ? <p>{data.name}</p> : <p>Location</p>}
+        <div className="container">
+          <div className="top">
+            <div className="location">
+              {(data.name) ? <p>{data.name}</p> : <div className="blanc">Location</div>}
+            </div>
+            <div className="temp">
+              {(data.main) ? <h1>{data.main.temp.toFixed() + "°C"}</h1> : <div className="blanc_h1">Temp</div>}
+            </div>
+            <div className="description">
+              {(data.weather) ? <p>{data.weather[0].main}</p> : <div className="blanc">Description</div>}
+            </div>
           </div>
-          <div className="temp">
-            {(data.main) ? <h1>{data.main.temp.toFixed() + "°C"}</h1> : <h1>Temp</h1>}
-          </div>
-          <div className="description">
-            {(data.weather) ? <p>{data.weather[0].main}</p> : <p>Description</p>}
-          </div>
+          {data.name != undefined && 
+            <div className="bottom">
+            <div className="feels">
+              {(data.main) ? <p>{data.main.feels_like.toFixed() + "°C"}</p> : <p>N/A</p>}
+              <span>Feels like</span>
+            </div>
+            <div className="humidity">
+              {(data.main) ? <p>{data.main.humidity + "%"}</p> : <p>N/A</p>}
+              <span>Humidity</span>
+            </div>
+            <div className="wind">
+              {(data.wind) ? <p>{Math.round(data.wind.speed) + "MS"}</p> : <p>N/A</p>}
+              <span>Wind speed</span>
+            </div>
+            </div>
+          }
         </div>
-        {data.name != undefined && 
-          <div className="bottom">
-          <div className="feels">
-            {(data.main) ? <p>{data.main.feels_like.toFixed() + "°C"}</p> : <p>N/A</p>}
-            <>Feels like</>
-          </div>
-          <div className="humidity">
-            {(data.main) ? <p>{data.main.humidity + "%"}</p> : <p>N/A</p>}
-            <>Humidity</>
-          </div>
-          <div className="wind">
-            {(data.wind) ? <p>{Math.round(data.wind.speed) + "MS"}</p> : <p>N/A</p>}
-            <>Wind speed</>
-          </div>
-          </div>
-        }
-      </div>
     </div>
   );
 }
